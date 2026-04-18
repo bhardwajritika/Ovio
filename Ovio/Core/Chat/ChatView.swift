1725776d136ccffa3668aae6daf1f1536178bfa4
Ritika <bhardwajritika11@gmail.com>
2026-04-19 01:06:07 +0530

    Chat View: Header and message input part is done.

---
+++ Ovio/Core/Chat/ChatView.swift
@@ -0,0 +1,64 @@
+//
+//  ChatView.swift
+//  Ovio
+//
+//  Created by Tarun Sharma on 18/04/26.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var messageText = ""
+    
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                // header
+                VStack {
+                    CircularProfileImageView(user: User.MOCK_USER, size: .xLarge)
+                    
+                    VStack (spacing: 4){
+                        Text(User.MOCK_USER.fullName)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                // messages
+                
+                
+            }
+            
+            // message input view
+            Spacer()
+            
+            ZStack (alignment: .trailing){
+                TextField("Message...", text: $messageText, axis: .vertical )
+                    .padding(12)
+                    .padding(.trailing, 48)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button {
+                    print("Send message")
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                        .padding(.horizontal)
+                }
+                
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatView()
+}
