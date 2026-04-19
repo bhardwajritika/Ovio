0981a6d45f2c364feb6f4f02ebaee87fce211482
Ritika <bhardwajritika11@gmail.com>
2026-04-19 17:04:56 +0530

    In chatView: Added ChatMessageCell and ChatBubble.

---
+++ Ovio/Core/Chat/ChatMessageCell.swift
@@ -0,0 +1,48 @@
+//
+//  ChatMessageCell.swift
+//  Ovio
+//
+//  Created by Tarun Sharma on 19/04/26.
+//
+
+import SwiftUI
+
+struct ChatMessageCell: View {
+    let isFromCurrentUser: Bool
+    
+    var body: some View {
+        HStack {
+            if isFromCurrentUser {
+                Spacer()
+                
+                Text("This is a sample message... longer than the other message . this is for testig...")
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemBlue))
+                    .foregroundColor(.white)
+                    .clipShape(ChatBubble(isFromCurrentUser: isFromCurrentUser))
+                    .frame(maxWidth: UIScreen.main.bounds.width / 1.5, alignment: .trailing)
+            } else {
+                HStack (alignment: .bottom, spacing: 8){
+                    CircularProfileImageView(user: User.MOCK_USER, size: .xxSmall)
+                    
+                    Text("This is also a smaple text.")
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray5))
+                        .foregroundColor(.black)
+                        .clipShape(ChatBubble(isFromCurrentUser: isFromCurrentUser))
+                        .frame(maxWidth: UIScreen.main.bounds.width / 1.75, alignment: .leading)
+                    
+                    Spacer()
+                }
+                
+            }
+        }
+        .padding(.horizontal, 8)
+    }
+}
+
+#Preview {
+    ChatMessageCell(isFromCurrentUser: true )
+}
